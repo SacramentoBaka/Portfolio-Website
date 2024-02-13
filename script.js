0c1134c8a75e9f5a045783f6f8cd3e7651976e1e
Sacramento <sacramentomakhubele@gmail.com>
2024-02-13 11:24:12 +0200

    code in javascript file, declared necessary parameters and variables.

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ window.onscroll = () => {
 
     header.classList.toggle('sticky', window.scrollY > 100);
 }
+
+let items = document.querySelectorAll('.slider .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let thumbnails = document.querySelectorAll('.thumbnail .item');
+
+// Config Param
+let countItem = items.length;
+let itemActive = 0;
+
