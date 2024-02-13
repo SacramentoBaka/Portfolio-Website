2a383bf2dd572866f22cfa9453243367e6873ce4
Sacramento <sacramentomakhubele@gmail.com>
2024-02-13 11:40:13 +0200

    The next button handled correctly in the JS file

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ window.onscroll = () => {
     header.classList.toggle('sticky', window.scrollY > 100);
 }
 
-let items = document.querySelectorAll('.slider .item');
+let items = document.querySelectorAll('.slider .list .item');
 let next = document.getElementById('next');
 let prev = document.getElementById('prev');
 let thumbnails = document.querySelectorAll('.thumbnail .item');
@@ -16,3 +16,22 @@ let thumbnails = document.querySelectorAll('.thumbnail .item');
 let countItem = items.length;
 let itemActive = 0;
 
+// EventHandling, next click
+next.onclick = function() {
+    itemActive += 1;
+    if(itemActive >= countItem){
+        itemActive = 0;
+    }
+    showSlider();
+}
+function showSlider(){
+    //Remove item active old
+    let itemActiveOld = document.querySelector('.slider .list .item.active');
+    let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
+    itemActiveOld.classList.remove('active');
+    thumbnailActiveOld.classList.remove('active');
+
+    // Active new item
+    items[itemActive].classList.add('active');
+    thumbnails[itemActive].classList.add('active');
+}
