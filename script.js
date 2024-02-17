d0dc17e94a2646b05491dd20dce3b8a99e921afa
Sacramento <sacramentomakhubele@gmail.com>
2024-02-17 11:21:39 +0200

    Active line done on the header

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 
+// Scroll Section Active link
+let Sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    Sections.forEach(sec => {
+        let top =window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+}
+
+
 let items = document.querySelectorAll('.slider .list .item');
 let next = document.getElementById('next');
 let prev = document.getElementById('prev');
